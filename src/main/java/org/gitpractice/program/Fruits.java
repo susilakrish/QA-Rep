6695c1096fa1862aee21c2c0fec331a00a955eaf
susilakrish <susilakrish2096@gmail.com>
2024-08-16 15:48:14 +0530

    6 First Git Own practice

---
+++ src/main/java/org/gitpractice/program/Fruits.java
@@ -0,0 +1,12 @@
+package org.gitpractice.program;
+
+public class Fruits {
+
+	public void orange() {
+		System.out.println("Orange");
+	}
+	
+	public void apple() {
+		System.out.println("Apple");
+	}
+}
